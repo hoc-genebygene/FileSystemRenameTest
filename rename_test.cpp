86f4b74c1651972868d4992af4f109ba1fddacf2
Ho Cheung <hoc@genebygene.com>
2017-10-05 14:47:24 -0500

    Add path

--- rename_test.cpp
+++ rename_test.cpp
@@ -3,7 +3,15 @@
 #include <sstream>
 
 int main(int argc, char * argv[]) {
-    char * temp_filename = strdup("XXXXXX");
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " /path/to/temp/dir" << std::endl;
+        return -1;
+    }
+
+    std::string temp_dir_str(argv[1]);
+    temp_dir_str = temp_dir_str + "/XXXXXX";
+
+    char * temp_filename = strdup(temp_dir_str.c_str());
     auto fd = mkstemp(temp_filename);
     if (fd == -1) {
         std::cerr << "Error generating temp file: " << std::strerror(errno) << std::endl;\
